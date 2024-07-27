ee3f95c9d447b30e44ad288a3a3372d6ded2c4fa
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-07-27 23:10:37 +0800

    3rd Challenge

---
+++ current_time.py
@@ -0,0 +1,6 @@
+import datetime
+
+now = datetime.datetime.now()
+
+print(now)
+print(now.strftime("%y-%m-%d %H:%M"))  #  format string
--- sum_of_first_positive_numbers.py
+++ sum_of_first_positive_numbers.py
@@ -2,7 +2,7 @@ sum = 0
 count = 0
 num = int(input("Enter how many numbers: "))
 
-while num != 0:
+while num != 0: #recursion
     sum += num
     count += 1
     num -= 1
