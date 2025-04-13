ca711de36542d7dea4f3ebf925b96e4f9c7322f8
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-13 09:24:33 +0800

    another exercise

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+input = int(input("Enter a number : "))
+
+
+for i in range(1,input+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+   
