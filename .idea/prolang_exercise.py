90b91dd5e438a7e52c0a7a4e1702f682307efb90
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-16 20:55:29 +0800

    add new exercise

---
+++ .idea/prolang_exercise.py
@@ -0,0 +1,18 @@
+# Assigning values to variables
+x = 10
+y = 5
+# Performing arithmetic operations
+sum_result = x + y
+product_result = x * y
+# Using a relational expression
+is_x_greater = x > y
+# Using a logical expression
+is_x_positive_and_greater = (x > 0) and (x > y)
+# Using an augmented assignment
+x += 3
+# Printing results
+print("Sum:", sum_result)
+print("Product:", product_result)
+print("Is x greater than y?", is_x_greater)
+print("Is x positive and greater than y?", is_x_positive_and_greater)
+print("Updated x:", x)
